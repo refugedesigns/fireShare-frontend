c440f5deb33091768e81add17677d5cbf56f7dc2
Erasmus Antwi <refugedesigns17@gmail.com>
2021-07-31 06:19:50 +0000

    completed form debugging

--- components/EmailForm.js
+++ components/EmailForm.js
@@ -1,44 +1,35 @@
-import { useState } from "react"
+import { useForm } from "react-hook-form";
 
 const EmailForm = (props) => {
-    const [emailFrom, setEmailFrom] = useState('')
-    const [emailTo, setEmailTo] = useState('')
-    const [error, setError] = useState(null)
+    const { register, handleSubmit, reset, formState: {errors}} = useForm()
     
-    
-    const emailFromHandler = event => {
-        setEmailFrom(event.target.value)
-    }
-
-    const emailToHandler = event => {
-        setEmailTo(event.target.value)
-    }
-    
-    const sendEmailHandler = (event) => {
-        event.preventDefault()
+    const sendEmailHandler = (data) => {
+ 
+        props.onSubmit(data)
         
-        if(emailFrom.trim() === '' || emailTo.trim() === '') {
-            setError("You inputs are incorrect, please check and try again.")
-            return
-        }
-
-        props.onSubmit({
-            emailFrom,
-            emailTo
-        })
-        
-        setEmailFrom('')
-        setEmailTo('')
-
+        reset()
     }
     
     return (
-        <form onSubmit={sendEmailHandler} className="p-4 flex flex-col items-center justify-center space-y-4 border-2 border-gray-500 my-4 rounded-md">
+        <form onSubmit={handleSubmit(sendEmailHandler)} className="p-4 flex flex-col items-center justify-center space-y-4 border-2 border-gray-500 my-4 rounded-md">
             <p className="text-gray-600">You can also send the link through mail directly</p>
-            <input onChange={emailFromHandler} type="email" placeholder="Email From" className="p-2 w-full rounded-md text-gray-600 focus:outline-none focus:ring focus:ring-gray-500" required/>
-            <input onChange={emailToHandler} type="email" placeholder="Email To" className="p-2 w-full rounded-md text-gray-600 focus:outline-none focus:ring focus:ring-gray-500" required />
-            {error && <p className="text-gray-600">{error}</p>}
-            {props.emailStatus && <p className="text-gray-600">{emailStatus}</p>}
+            <input {...register("emailFrom" , {
+                required: true,
+                pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: "Entered value does not match email format"
+                }
+                })} type="email" placeholder="Email From" className="p-2 w-full rounded-md text-gray-600 focus:outline-none focus:ring focus:ring-gray-500" required/>
+            {errors.emailFrom && <p className="text-gray-600">{errors.emailFrom.message}</p>}
+            <input {...register("emailTo", {
+                required: true,
+                pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: "Entered value does not match email format"
+                }
+                })} type="email" placeholder="Email To" className="p-2 w-full rounded-md text-gray-600 focus:outline-none focus:ring focus:ring-gray-500" required />
+            {errors.emailTo && <p className="text-gray-600">{errors.emailTo.message}</p>}
+            {props.emailStatus && <p className="text-gray-600">{props.emailStatus}</p>}
             <button type="submit" className="button w-1/2">Email</button>
         </form>
     )
