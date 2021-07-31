37bfe3dde5fa8e8f07df669c6fb61d97470436c2
Erasmus Antwi <refugedesigns17@gmail.com>
2021-07-31 03:22:28 +0000

    testing form

--- components/EmailForm.js
+++ components/EmailForm.js
@@ -6,6 +6,14 @@ const EmailForm = ({id}) => {
     const [error, setError] = useState(null)
     const [emailStatus, setEmailStatus] = useState("")
     
+    const emailFromHandler = event => {
+        setEmailFrom(event.target.value)
+    }
+
+    const emailToHandler = event => {
+        setEmailTo(event.target.value)
+    }
+    
     const sendEmailHandler = async(event) => {
         event.preventDefault()
         
@@ -40,8 +48,8 @@ const EmailForm = ({id}) => {
     return (
         <form onSubmit={sendEmailHandler} className="p-4 flex flex-col items-center justify-center space-y-4 border-2 border-gray-500 my-4 rounded-md">
             <p className="text-gray-600">You can also send the link through mail directly</p>
-            <input onChange={(event) => setEmailFrom(event.target.value)} type="email" placeholder="Email From" className="p-2 w-full rounded-md text-gray-600 focus:outline-none focus:ring focus:ring-gray-500" required/>
-            <input onChange={(event) => setEmailTo(event.target.value)} type="email" placeholder="Email To" className="p-2 w-full rounded-md text-gray-600 focus:outline-none focus:ring focus:ring-gray-500" required />
+            <input onChange={emailFromHandler} type="email" placeholder="Email From" className="p-2 w-full rounded-md text-gray-600 focus:outline-none focus:ring focus:ring-gray-500" required/>
+            <input onChange={emailToHandler} type="email" placeholder="Email To" className="p-2 w-full rounded-md text-gray-600 focus:outline-none focus:ring focus:ring-gray-500" required />
             {error && <p className="text-gray-600">{error}</p>}
             {emailStatus && <p className="text-gray-600">{emailStatus}</p>}
             <button type="submit" className="button w-1/2">Email</button>
